12118b24be4e04c916ce8bec8214d4ea868c40c8
Rushi K <rushikool12@gmail.com>
2023-01-09 14:43:36 -0500

    new tutorial

---
+++ PowerPoint/create_slide.py
@@ -0,0 +1,12 @@
+from pptx import Presentation
+
+"Create PPT presentation to add slides to"
+"Similar concept to tkinter where you add widgets to the main screen"
+
+pr1 = Presentation()
+
+
+
+"Refers to the layouts "
+
+pr1.save("First_Presentation.pptx")
